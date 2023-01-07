b5d62d5893f6539e6c0730d136f28c6ee501f687
lucasnerism <lucasnerism@gmail.com>
2023-01-07 01:02:22 -0300

    Ajustes finos e otimização

--- script.js
+++ script.js
@@ -1,83 +1,65 @@
-function selecionarPrato(prato) {
-  const selecionadoantes = document.querySelector('.prato .selecionado')
+function selecionar(anterior, opcao) {
+  const selecionadoantes = document.querySelector(`.${anterior} .selecionado`);
 
   if (selecionadoantes !== null) {
-    selecionadoantes.classList.remove('selecionado')
+    selecionadoantes.classList.remove('selecionado');
   }
 
-  prato.classList.add('selecionado')
+  opcao.classList.add('selecionado');
 
-  ativarBotao()
-}
-
-function selecionarBebida(bebida) {
-  const selecionadoantes = document.querySelector('.bebida .selecionado')
-
-  if (selecionadoantes !== null) {
-    selecionadoantes.classList.remove('selecionado')
-  }
-
-  bebida.classList.add('selecionado')
-
-  ativarBotao()
-}
-
-function selecionarSobremesa(sobremesa) {
-  const selecionadoantes = document.querySelector('.sobremesa .selecionado')
-
-  if (selecionadoantes !== null) {
-    selecionadoantes.classList.remove('selecionado')
-  }
-
-  sobremesa.classList.add('selecionado')
-
-  ativarBotao()
+  ativarBotao();
 }
 
 function ativarBotao() {
-  let quantidade = document.getElementsByClassName('selecionado').length
+  const quantidadeselecionados = document.getElementsByClassName('selecionado').length;
+  //vi no sonarqube pra evitar "magic numbers" então fiz essa variavel aqui
+  const minselecionados = 3;
 
-  if (quantidade == 3) {
-    document.querySelector('footer button').classList.add('verde')
-    document.querySelector('footer button div').classList.add('fecharpedido')
-    document.querySelector('footer button div').innerHTML = 'Fechar Pedido'
-    document.querySelector('footer button').removeAttribute('disabled')
+  if (quantidadeselecionados === minselecionados) {
+    document.querySelector('footer button').classList.add('verde');
+    document.querySelector('footer button div').classList.add('fecharpedido');
+    document.querySelector('footer button div').innerHTML = 'Fechar Pedido';
+    document.querySelector('footer button').removeAttribute('disabled');
   }
 }
 
+let mensagem;
+
 function confirmarPedido() {
-  document.querySelector('.overlay').style.display = 'initial'
-  const prato = document.querySelector('.prato .selecionado h2').innerHTML
-  const bebida = document.querySelector('.bebida .selecionado h2').innerHTML
+  document.querySelector('.overlay').style.display = 'initial';
+  const prato = document.querySelector('.prato .selecionado h2').innerHTML;
+  const bebida = document.querySelector('.bebida .selecionado h2').innerHTML;
   const sobremesa = document.querySelector(
     '.sobremesa .selecionado h2'
-  ).innerHTML
-  const preco1 = document.querySelector('.prato .selecionado .preco').innerHTML
+  ).innerHTML;
+  const preco1 = document.querySelector('.prato .selecionado .preco').innerHTML;
 
-  const preco2 = document.querySelector('.bebida .selecionado .preco').innerHTML
+  const preco2 = document.querySelector('.bebida .selecionado .preco').innerHTML;
 
   const preco3 = document.querySelector(
     '.sobremesa .selecionado .preco'
-  ).innerHTML
+  ).innerHTML;
 
-  document.querySelector('#item1 p:first-child').innerHTML = prato
-  document.querySelector('#item1 p:last-child').innerHTML = preco1
-  document.querySelector('#item2 p:first-child').innerHTML = bebida
-  document.querySelector('#item2 p:last-child').innerHTML = preco2
-  document.querySelector('#item3 p:first-child').innerHTML = sobremesa
-  document.querySelector('#item3 p:last-child').innerHTML = preco3
+  document.querySelector('#item1 p:first-child').innerHTML = prato;
+  document.querySelector('#item1 p:last-child').innerHTML = preco1;
+  document.querySelector('#item2 p:first-child').innerHTML = bebida;
+  document.querySelector('#item2 p:last-child').innerHTML = preco2;
+  document.querySelector('#item3 p:first-child').innerHTML = sobremesa;
+  document.querySelector('#item3 p:last-child').innerHTML = preco3;
 
-  valor1 = valores(preco1)
-  valor2 = valores(preco2)
-  valor3 = valores(preco3)
-  valortotal = valor1 + valor2 + valor3
+  const valor1 = valores(preco1);
+  const valor2 = valores(preco2);
+  const valor3 = valores(preco3);
+  const valortotal = valor1 + valor2 + valor3;
+  //vi no sonarqube pra evitar "magic numbers" então fiz essa variavel aqui
+  const decimais = 2;
 
-  preco = valortotal.toFixed(2)
-  preco = preco.replace('.', ',')
+  let preco = valortotal.toFixed(decimais);
+  preco = preco.replace('.', ',');
 
-  document.querySelector('#total').innerHTML = 'R$ ' + preco
+  document.querySelector('#total').innerHTML = 'R$ ' + preco;
 
-  const mensagem =
+  mensagem =
     'Olá, gostaria de fazer o pedido:\n- Prato: ' +
     prato +
     '\n- Bebida: ' +
@@ -85,27 +67,27 @@ function confirmarPedido() {
     '\n- Sobremesa: ' +
     sobremesa +
     '\nTotal: R$ ' +
-    preco
-
-  return mensagem
+    preco;
 }
 
 function valores(string) {
-  let numero = string.replace(/[^0-9]/g, '')
-  return parseInt(numero) / 100
+  const numero = string.replace(/[^0-9]/g, '');
+  //vi no sonarqube pra evitar "magic numbers" então fiz essa variavel aqui
+  const ajustedecimal = 100;
+  return parseInt(numero) / ajustedecimal;
 }
 
-function enviarPedido(mensagem) {
-  const nome = prompt('Qual o seu nome?')
-  const endereco = prompt('Qual o seu endereço?')
-  mensagem = mensagem + '\n\nNome: ' + nome + '\nEndereço: ' + endereco
+function enviarPedido() {
+  const nome = prompt('Qual o seu nome?');
+  const endereco = prompt('Qual o seu endereço?');
+  mensagem = mensagem + '\n\nNome: ' + nome + '\nEndereço: ' + endereco;
 
-  const wapp = 'https://wa.me/5531992432456?text=' + encodeURI(mensagem)
-  window.open(wapp)
+  const wapp = 'https://wa.me/5531992432456?text=' + encodeURI(mensagem);
+  window.open(wapp);
   //já vou deixar o console.log aqui pra facilitar a checagem da mensagem
   //console.log(mensagem)
 }
 
 function cancelar() {
-  document.querySelector('.overlay').style.display = 'none'
+  document.querySelector('.overlay').style.display = 'none';
 }
